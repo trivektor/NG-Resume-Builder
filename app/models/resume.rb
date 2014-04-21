a7792cad266d74fc478797583f7fee4a5b2c82c1
trivektor <trivektor@gmail.com>
2014-04-20 17:26:36 -0700

    edit button

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -4,4 +4,10 @@ class Resume < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
 
+  def as_json(options={})
+    super.merge({
+      url: "/resumes/#{id}/edit"
+    })
+  end
+
 end
